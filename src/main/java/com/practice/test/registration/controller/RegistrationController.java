247219f2eafa6927d794102eed8f359f33950db9
Dhiraj Begani <43821291+dhirajbegani@users.noreply.github.com>
2020-01-22 00:39:47 +0800

    Changes to add new format specific mapping

--- src/main/java/com/practice/test/registration/controller/RegistrationController.java
+++ src/main/java/com/practice/test/registration/controller/RegistrationController.java
@@ -1,15 +1,40 @@
 package com.practice.test.registration.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegistrationController {
 
 	@RequestMapping("/")
-    public String welcome() {
-        return "Welcome to registration page"; 
-    }
- 
+	public String welcome() {
+		return "Welcome to registration page";
+	}
 
+	@GetMapping(value = "/requestLogin/json", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public Map<String, Object> getJsonLoginCredntials(@RequestParam Optional<String> userName,
+			@RequestParam Optional<String> password) {
+		Map<String, Object> loginDetails = new HashMap<>();
+
+		loginDetails.put("User Name", userName.orElseGet(() -> "not provided"));
+		loginDetails.put("Password", password.orElseGet(() -> "not provided"));
+
+		return loginDetails;
+	}
+
+	@GetMapping(value = "/requestLogin/html", produces = MediaType.TEXT_HTML_VALUE)
+	@ResponseBody
+	public String getHtmlLoginCredntials(@RequestParam Optional<String> userName,
+			@RequestParam Optional<String> password) {
+		return  "User Name: " + userName.orElseGet(() -> "not provided") + "<br> Password: " + password.orElseGet(() -> "not provided");	
+}
 }
